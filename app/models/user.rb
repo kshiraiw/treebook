a0bc3d3750dcac8e7e341913bdd94e54d14da64a
Kiyomi Li <kshiraiw@gmail.com>
2014-11-24 19:49:43 -0800

    Updated frontend for the new authentication system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+	has_many :statuses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+  	"#{first_name} #{last_name}"
+  end
 
  end
